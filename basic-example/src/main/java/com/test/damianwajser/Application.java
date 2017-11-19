486c09e2ee227e91a8bb45b123591cc6b83c37d7
damianwajser <wajser@gmail,com>
2017-11-19 12:41:46 -0300

    clean

--- basic-example/src/main/java/com/test/damianwajser/Application.java
+++ basic-example/src/main/java/com/test/damianwajser/Application.java
@@ -1,19 +1,15 @@
 package com.test.damianwajser;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan({"com.github.damianwajser"})
 @SpringBootApplication
-@ComponentScan(basePackages={"com.github.damianwajser","com.prismamp.todopago"})
-@EnableAutoConfiguration
-@EnableCaching
-public class MsParametersApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MsParametersApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
