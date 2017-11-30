8d3136d9b3d17ed35313ba04ad568774d70f9d65
damianwajser <wajser@gmail,com>
2017-11-30 20:26:31 -0300

    example

--- basic-example/src/main/java/com/test/damianwajser/controllers/ExapmleResource.java
+++ basic-example/src/main/java/com/test/damianwajser/controllers/ExapmleResource.java
@@ -2,6 +2,8 @@ package com.test.damianwajser.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,12 +12,17 @@ import com.test.damianwajser.model.Example;
 @RestController
 @RequestMapping("/example")
 public class ExapmleResource {
-	
+
 	@GetMapping("/{id}")
 	public Example getById(@PathVariable("id") Integer id) {
 		Example example = new Example();
 		example.setDescription("description");
 		return example;
 
+	}
+
+	@PostMapping
+	public Example post(@RequestBody Example example) {
+		return example;
 	}
 }
